12994637db312acc32465baf25c4ea9f3de595af
Yago B. Fontoura <33014958+YagoFontoura@users.noreply.github.com>
2022-09-28 16:53:02 -0300

    Update pesquisar.py

--- pesquisar.py
+++ pesquisar.py
@@ -5,7 +5,7 @@ def pesquisar():
     except ImportError:  
         print("No module named 'google' found") 
 
-    query = "modem da vivo com problema de conexão"
+    query = "Youtube"
         
     for j in search(query, tld="co.in", num=30, stop=30, pause=2): 
         print(j) 
